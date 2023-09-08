7a62f1b634898ac5d8b581d6f00b8a906e71c534
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-09-08 22:56:09 +0530

    frontend

---
+++ frontend/script/signup.js
@@ -0,0 +1,23 @@
+const formEl = document.querySelector('form');
+const nameEl = document.querySelector('#name');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+
+formEl.addEventListener('submit', async (evnt)=>{
+    evnt.preventDefault();
+    let res = await fetch('https://chat-room-app-s6tv.onrender.com/user/register',{
+        body: JSON.stringify({name: nameEl.value, email: emailEl.value, password: passwordEl.value}),
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    let result = res.ok
+    res = await res.json();
+    if(result){
+        alert('Sign up Successful');
+        window.location.href='signin.html';
+    }else{
+        alert(res.msg);
+    }
+})
